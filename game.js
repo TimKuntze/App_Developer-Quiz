2ffe8f8b097df1755f099f88617d7a6f22d366d0
Tim Kuntze <timkuntze@Tims-MacBook-Pro.local>
2020-08-18 22:13:07 +0200

    update

--- game.js
+++ game.js
@@ -26,66 +26,59 @@ let allQuestions = [
         'right-answer': 3
     },
     {   //question#4
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
+        'question': 'What does CSS stand for?',
+        'answer_1': 'Colorful Style Sheets',
+        'answer_2': 'Cascading Style Sheets',
+        'answer_3': 'Creative Style Sheets',
+        'answer_4': 'Computer Style Sheets',
+        'right-answer': 2
     },
     {   //question#5
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
+        'question': 'Where to refer to an external style sheet?',
+        'answer_1': 'Body Section',
+        'answer_2': 'Head Section',
+        'answer_3': 'At the very beginning',
+        'answer_4': 'At the very end',
+        'right-answer': 2
     },
     {   //question#6
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
+        'question': 'Which CSS property controls the text size?',
+        'answer_1': 'text-style',
+        'answer_2': 'font-style',
+        'answer_3': 'text-size',
+        'answer_4': 'font-size',
+        'right-answer': 4
     },
     {   //question#7
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
+        'question': 'How do you write "Hello World" in an alert box?',
+        'answer_1': 'msg("Hello World")',
+        'answer_2': 'alert("Hello World")',
+        'answer_3': 'alertBox("Hello World")',
+        'answer_4': 'msgBox("Hello World")',
+        'right-answer': 2
     },
     {   //question#8
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
+        'question': 'What occurs when the user clicks on an HTML element?',
+        'answer_1': 'onmouseover',
+        'answer_2': 'onchange',
+        'answer_3': 'onclick',
+        'answer_4': 'onmouseclick',
+        'right-answer': 3
     },
     {   //question#9
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
-    },
-    {   //question#10
-        'question': 'CSS1?',
-        'answer_1': 'CSS1a',
-        'answer_2': 'w',
-        'answer_3': 'Trr',
-        'answer_4': 'rt',
-        'right-answer': 1
+        'question': 'Which data type is used for a variable to store text?',
+        'answer_1': 'myString',
+        'answer_2': 'Txt',
+        'answer_3': 'String',
+        'answer_4': 'txtString',
+        'right-answer': 3
     }
 ];
 
 let right_answer;
 let question_number = 0;
 let progress = 0;
+let blockSelection = false;
 
 function hideElements() {
     document.getElementById('text').style.display = 'none';
@@ -102,7 +95,26 @@ function startQuiz() {
     loadQuestion();
 }
 
+function navbarSelection() {
+    if (question_number > 2) {
+        document.getElementById('nav-bar-html').classList.add('d-none');
+        document.getElementById('nav-bar-css').classList.remove('d-none');
+    }
+    if (question_number > 5) {
+        document.getElementById('nav-bar-html').classList.add('d-none');
+        document.getElementById('nav-bar-css').classList.add('d-none');
+        document.getElementById('nav-bar-js').classList.remove('d-none');
+    } 
+    if (question_number > 7) {
+        document.getElementById('nav-bar-html').classList.add('d-none');
+        document.getElementById('nav-bar-css').classList.add('d-none');
+        document.getElementById('nav-bar-js').classList.add('d-none');
+        document.getElementById('nav-bar-java').classList.remove('d-none');
+    } 
+}
+
 function loadQuestion() {
+    progressBar();
     document.getElementById('question').innerHTML = allQuestions[question_number]['question'];
     document.getElementById('answer1').innerHTML = allQuestions[question_number]['answer_1'];
     document.getElementById('answer2').innerHTML = allQuestions[question_number]['answer_2'];
@@ -112,20 +124,24 @@ function loadQuestion() {
 }
 
 function nextQuestion() {
-
+    blockSelection = false;
     if (question_number == allQuestions.length) {
         finishQuiz();
     } else {
-        progress = Math.round((question_number / allQuestions.length) * 100);
         question_number = question_number + 1;
-        //document.getElementById('progress-bar').innerHTML = progress + '%';
-        //document.getElementById('progress-bar').style.width = progress + '%';
+        backQuestion();
         loadQuestion();
+        navbarSelection();
     }
 }
 
-function finishQuiz() {
+function progressBar() {
+    progress = Math.round((question_number / allQuestions.length) * 100);
+    //document.getElementById('progress-bar').innerHTML = progress + '%';
+    document.getElementById('progress-bar').style.width = progress + '%';
+}
 
+function finishQuiz() {
     if (question_number == allQuestions.length) {
         document.getElementById('quizcontainer').classList.add('d-none');
         document.getElementById('quizdone').classList.remove('d-none');
@@ -135,23 +151,24 @@ function finishQuiz() {
 }
 
 function answer(a) {
+    if (blockSelection == false) {
+        blockSelection = true;
+        if (a == right_answer) { //right answer
+            document.getElementById('answer-container').classList.add('answer-correct');
+            //show next button 
+            document.getElementById('next').classList.remove('d-none');
+        } else {
 
-    if (a == right_answer) { //right answer
-        document.getElementById('answer-container').classList.add('answer-correct');
-        //show next button 
-        document.getElementById('next').classList.remove('d-none');
-
-
-    } else {
-
-        document.getElementById('answer-container').classList.add('answer-wrong');
+            document.getElementById('answer-container').classList.add('answer-wrong');
+        }
     }
 }
 
 function backQuestion() {
-    if (question_number > 1) {
+    if (question_number > 0) {
         document.getElementById('back').classList.remove('d-none');
     } else {
         document.getElementById('back').classList.add('d-none');
     }
 }
+
